d12c95096f2ad78c70dc54e31eee79d891969af9
Vinay Y S <vinay@vys.in>
2012-10-08 22:01:05 +0530

    Adding sysctl conf to increase ephemeral ports
    Signed-off-by: Vinay Y S <vinay@vys.in>

--- scripts/pkt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while true ; do 
-start=`ifconfig lo | grep "TX packets" | awk '{print $2 }' | cut -d: -f2`
-sleep 1
-end=`ifconfig lo | grep "TX packets" | awk '{print $2 }' | cut -d: -f2`
-let pktsec=$end-$start
-echo "$pktsec / second"
-done
